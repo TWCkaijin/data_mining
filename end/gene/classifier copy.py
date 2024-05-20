9a1ecfc19623058d0f43df7d4ee18099bb09738a
kaijin <kaijinkaijin2@gmail.com>
2024-05-20 12:53:02 +0800

    UD

--- end/gene/classifier copy.py
+++ end/gene/classifier copy.py
@@ -7,8 +7,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 # gloabl
 model_name = "gene"
 
-label_dict = dict()
-
 class Color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -21,7 +19,26 @@ class Color:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
+def read_test()->tuple:
+    label_dict = dict()
+    FT = open(f'{os.getcwd()}/{model_name}/train_data.csv','r')
+    data = np.genfromtxt(FT,delimiter=',',dtype='float32',filling_values=0.0)[1:]
+
+    FL = open(f'{os.getcwd()}/{model_name}/train_label.csv','r')
+    label = np.genfromtxt(FL,delimiter=',',dtype='str')
+    label = pd.DataFrame(label)
+    label = label.drop(0,axis=1).to_numpy()[1:]
+    for i in range(len(label)):
+        if label[i][0] not in label_dict:
+            label_dict[label[i][0]] = len(label_dict)
+        label[i][0] = label_dict[label[i][0]]
+
+
+
+    return Data_preprocessing(data,label)
+
 def read_train()->tuple:
+    label_dict = dict()
     FT = open(f'{os.getcwd()}/{model_name}/train_data.csv','r')
     train_data = np.genfromtxt(FT,delimiter=',',dtype='float32',filling_values=0.0)[1:]
 
@@ -46,16 +63,15 @@ def Data_preprocessing(data,label)->tuple:
     # Missing value
     for i in range(len(temp.columns)):
         if (temp.loc[:,i] == 0).all():
-
             temp = temp.drop(i,axis=1)
     # Normalization
     data = temp.to_numpy()
     data = (data - np.min(data,axis=0))/((np.max(data,axis=0)-np.min(data,axis=0)) )
     return data , label
-    
+
 def sampling(train_data,train_labels,rate,used):
 
-    total_range = np.setdiff1d(len(train_data),used)
+    total_range = np.setdiff1d(np.arange(len(train_data)),used)
     SI = np.random.choice(total_range,int(len(train_data)*rate),replace=False)
     valid_data , valid_labels = train_data[SI] , train_labels[SI]
     #train_data , train_labels = np.delete(train_data,SI,axis=0) , np.delete(train_labels,SI,axis=0)
@@ -65,62 +81,94 @@ def sampling(train_data,train_labels,rate,used):
 
     return train_data , train_labels , valid_data , valid_labels , np.concatenate((SI,used),axis=0) , SI
 
+
+def Data_aug(data,label):
+
+    ans_quan = dict()
+    ans_sheet = [[-1],[-1],[-1]] #存放資料的index
+
+    for i in range(len(label)):
+        if(label[i][0] not in ans_quan):
+            ans_quan.update({label[i][0]:1})
+            ans_sheet[label[i][0]][0] = i
+        else:
+            ans_quan[label[i][0]]+=1
+            #print(ans_sheet[label[i][0]])
+            ans_sheet[label[i][0]].append(i)
+    
+
+    ans_avg = [np.mean(data[ans_sheet[x]],axis=0) for x in range(3)] # ans_avg 第一維度是類別(3) 第二維度是特徵(20242)
+
+    for i in range(len(ans_sheet)):
+        add_quan = max(ans_quan.values())-ans_quan[i]
+        if add_quan <= 0:
+            continue
+        for j in np.random.choice(a=ans_sheet[i],size = add_quan):
+            temp = np.array([(ans_avg[i] + data[j])*np.random.random(1)])
+            
+            data = np.concatenate((data,temp),axis=0)
+            label = np.concatenate((label,np.array([[i]])),axis=0)
+    return data,label
+
+
 if __name__ == '__main__':
-    Clock_start = time.time()  
+    
     used = np.array([])
     # Read data
-
+    rate = 0.2 
+    Clock_start = time.time()  
     train_data , train_labels = read_train()
 
-    print(f'Read time :{time.time()-Clock_start}')
-    rate = float(input("Enter the rate of validation data(0.X): "))
+    #增強
+    train_data,train_labels = Data_aug(train_data,train_labels)
+    print(train_data.shape,train_labels.shape)
+    
+    #隨機抽樣
+    train_data, train_labels, valid_data, valid_labels, SI, used= sampling(train_data,train_labels,rate,used)
 
+    #raise Exception("Stop")
+    train_labels = tf.keras.utils.to_categorical(train_labels,num_classes=3)
+    valid_labels = tf.keras.utils.to_categorical(valid_labels,num_classes=3)
+
+    test_data,test_label = read_test()
+    test_label = tf.keras.utils.to_categorical(test_label,num_classes=5)
+
+    print(f'Read time :{time.time()-Clock_start}')
+
+    
     from keras import layers as kl
     model = tf.keras.Sequential([
         kl.InputLayer(input_shape=(train_data.shape[1],)),
-        kl.Dense(512, activation='relu',kernel_regularizer='l1'),
-        kl.Dense(512, activation='softmax',kernel_regularizer='l1'),
+        kl.Dense(4096, activation='softmax'),
+        kl.Dense(512, activation='sigmoid'),
         kl.Dropout(0.2),
-        kl.Dense(512, activation='sigmoid',kernel_regularizer='l1'),
-        kl.Dense(1024, activation='softmax',kernel_regularizer='l2'),
-        kl.Dropout(0.2),
-        kl.Dense(1024, activation='relu',kernel_regularizer='l1'),
+        kl.Dense(512, activation='softmax'),
+        kl.Dense(512, activation='sigmoid'),
         kl.Dropout(0.2),
         kl.Dense(3, activation='softmax'),
     ])
-    train_data, train_labels, valid_data, valid_labels, SI, used= sampling(train_data,train_labels,rate,used)
-    train_labels = tf.keras.utils.to_categorical(train_labels,num_classes=3)
-    valid_labels = tf.keras.utils.to_categorical(valid_labels,num_classes=3)
+
     while(True):
-        
-        # 抽樣
-        
 
-
-        #轉換標籤為"標籤位置"的形式
-        
-        print(valid_labels)
-        
-
-        
         model.summary()
         input("Start training")
 
         model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-        model.fit(train_data, train_labels,validation_split=0.2, epochs=40, verbose=1, shuffle=True)
+        model.fit(train_data, train_labels,epochs=40, verbose=1, shuffle=True)
         #loaded_model = tf.keras.models.load_model('model.h5')
 
-        valid_predict = np.array(model.predict(valid_data, verbose=2))
+        valid_predict = np.array(model.predict(test_data, verbose=2))
         f=  open (f'{os.getcwd()}/{model_name}/valid_pred.txt','w')
         f.write(str(list(valid_predict)))
         f.close()
-        print("Validation Accuracy: ",sum(valid_predict.argmax(axis=1)==valid_labels.argmax(axis=1))/len(valid_labels))
-        #model.layers[0].trainable = False
+
+        print("Validation Accuracy: ",sum(valid_predict.argmax(axis=1)==test_label.argmax(axis=1))/len(test_label))
+
         if(input("keep training? (y/n): ") != 'y'):
             break
         train_data, train_labels, valid_data, valid_labels, SI, used= sampling(train_data,train_labels,rate,used)
 
-    print(f'Time taken: {time.time()-Clock_start}')
+
     cond = input("save model? (y/n): ")
     if(cond == 'y' or cond == 'Y' ):
         model.save(f'{os.getcwd()}/{model_name}/{input("Enter the model name: ")}')
