2a675d4801e2a5e9f0a9375f2f521b36c95a6447
kaijin <kaijinkaijin2@gmail.com>
2024-05-21 12:36:59 +0800

    UD

--- end/Arr/classifier.py
+++ end/Arr/classifier.py
@@ -7,7 +7,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' #TF 通知設定
 os.environ['CUDA_VISIBLE_DEVICES'] = '0' #TF GPU 參數設定
 
 # gloabl
-model_name = "gene"
+model_name = "Arr"
+
 
 def read_test()->tuple:
     label_dict = dict()
@@ -15,31 +16,18 @@ def read_test()->tuple:
     data = np.genfromtxt(FT,delimiter=',',dtype='float32',filling_values=0.0)[1:]
 
     FL = open(f'{os.getcwd()}/{model_name}/test_label.csv','r')
-    label = np.genfromtxt(FL,delimiter=',',dtype='str')
-    label = pd.DataFrame(label)
-    label = label.drop(0,axis=1).to_numpy()[1:]
-    for i in range(len(label)):
-        if label[i][0] not in label_dict:
-            label_dict[label[i][0]] = len(label_dict)
-        label[i][0] = label_dict[label[i][0]]
-
-
+    label = np.genfromtxt(FL,dtype='int32')
+    label = label-1
 
     return Data_preprocessing(data,label)
 
 def read_train()->tuple:
-    label_dict = dict()
     FT = open(f'{os.getcwd()}/{model_name}/train_data.csv','r')
-    train_data = np.genfromtxt(FT,delimiter=',',dtype='float32',filling_values=0.0)[1:]
+    train_data = np.genfromtxt(FT,delimiter=',',dtype='float32',filling_values=0.0)
 
     FL = open(f'{os.getcwd()}/{model_name}/train_label.csv','r')
-    train_label = np.genfromtxt(FL,delimiter=',',dtype='str')
-    train_label = pd.DataFrame(train_label)
-    train_label = train_label.drop(0,axis=1).to_numpy()[1:]
-    for i in range(len(train_label)):
-        if train_label[i][0] not in label_dict:
-            label_dict[train_label[i][0]] = len(label_dict)
-        train_label[i][0] = label_dict[train_label[i][0]]
+    train_label = np.genfromtxt(FL,dtype='int32')
+    train_label = train_label - 1
 
 
 
@@ -51,12 +39,18 @@ def Data_cleaning():
 def Data_preprocessing(data,label)->tuple:
     temp = pd.DataFrame(data)
 
+    # Missing value delete
+    '''
+    for i in range(len(temp.columns)):
+        if (temp.loc[:,i] == 0).all():
+            temp = temp.drop(i,axis=1)
+    '''
     # Normalization
     data = temp.to_numpy()
     data = (data - np.min(data,axis=0))/((np.max(data,axis=0)-np.min(data,axis=0)) ) 
     # Missing value handling
     data[np.isnan(data)] = 0
-    return data , label
+    return np.array(data) , np.array(label)
 
 def sampling(train_data,train_labels,rate,used):
 
@@ -67,25 +61,21 @@ def sampling(train_data,train_labels,rate,used):
 
 
 def Data_aug(data,label):
+
     ans_quan = dict()
-    ans_sheet = [[-1] for x in range(3)] #存放資料的index
-
+    ans_sheet = [[-1] for x in range(8)] #存放資料的index
+    print(data.shape,label.shape)
     for i in range(len(label)):
-        if(label[i][0] not in ans_quan):
-            ans_quan.update({label[i][0]:1})
-            ans_sheet[label[i][0]][0] = i
+        if(label[i] not in ans_quan):
+            ans_quan.update({label[i]:1})
+            ans_sheet[label[i]] = [i]
         else:
-            ans_quan[label[i][0]]+=1
-            #print(ans_sheet[label[i][0]])
-            ans_sheet[label[i][0]].append(i)
+            ans_quan[label[i]]+=1
+            ans_sheet[label[i]].append(i)
     
-
-    ans_avg = [np.mean(data[ans_sheet[x]],axis=0) for x in range(3)] # ans_avg 第一維度是類別(3) 第二維度是特徵(20242)
-    farest_data = []
-    for i in range(len(ans_avg)):
-        distances = [distance(data[ans_sheet[i][j]],ans_avg[i]) for j in range(len(ans_sheet[i]))]
-        farest_data.append(max(distances))
-    np.savetxt(f'{os.getcwd()}/{model_name}/model/ans_avg.csv',farest_data)
+    #print(ans_quan)
+    #print(ans_sheet)
+    ans_avg = [np.mean(data[ans_sheet[x]],axis=0) for x in range(8)] # ans_avg 第一維度是類別(3) 第二維度是特徵(20242)
 
     for i in range(len(ans_sheet)):
         add_quan = max(ans_quan.values())-ans_quan[i]
@@ -94,13 +84,10 @@ def Data_aug(data,label):
         for j in np.random.choice(a=ans_sheet[i],size = add_quan):
             temp = np.array([(ans_avg[i] + data[j])*0.5])
             data = np.concatenate((data,temp),axis=0)
-            label = np.concatenate((label,np.array([[i]])),axis=0)
+            label = np.concatenate((label,np.array([i])),axis=0)
 
     return data,label
 
-
-def distance(boj1,obj2):
-    return np.sqrt(np.sum(np.power(boj1-obj2,2)))
 
 if __name__ == '__main__':
     
@@ -117,11 +104,11 @@ if __name__ == '__main__':
     train_data, train_labels, valid_data, valid_labels, SI, used= sampling(train_data,train_labels,rate,used)
 
     #raise Exception("Stop")
-    train_labels = tf.keras.utils.to_categorical(train_labels,num_classes=3)
-    valid_labels = tf.keras.utils.to_categorical(valid_labels,num_classes=3)
+    train_labels = tf.keras.utils.to_categorical(train_labels,num_classes=8)
+    valid_labels = tf.keras.utils.to_categorical(valid_labels,num_classes=8)
 
     test_data,test_label = read_test()
-    test_label = tf.keras.utils.to_categorical(test_label,num_classes=5)
+    test_label = tf.keras.utils.to_categorical(test_label,num_classes=13)
 
     print(f'Read time :{time.time()-Clock_start}')
 
@@ -130,7 +117,7 @@ if __name__ == '__main__':
     from tensorflow.keras.callbacks import EarlyStopping
 
     model = tf.keras.Sequential([
-        Dense(1024, input_dim=train_data.shape[1],activation='relu'),
+        Dense(512, input_dim=train_data.shape[1],activation='relu'),
         BatchNormalization(),
         Dropout(0.5),
 
@@ -142,7 +129,13 @@ if __name__ == '__main__':
         BatchNormalization(),
         Dropout(0.5),
 
-        Dense(3, activation='softmax'),
+
+        Dense(128, activation='relu'),
+        BatchNormalization(),
+        Dropout(0.5),
+
+
+        Dense(8, activation='softmax'),
     ])
 
     while(True):
@@ -152,7 +145,7 @@ if __name__ == '__main__':
 
         early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
         model.fit(train_data, train_labels,epochs=40, verbose=1, shuffle=True)
         #loaded_model = tf.keras.models.load_model('model.h5')
 
@@ -164,8 +157,18 @@ if __name__ == '__main__':
 
         loss, acc = model.evaluate(valid_data, valid_labels, verbose=2)
         print("Validation loss: ",loss,"   Accuracy",acc)
-        print("Test Accuracy: ",sum(valid_predict.argmax(axis=1)==test_label.argmax(axis=1))/len(test_label))
+        print(valid_predict,test_label)
+        correct = 0
+        for i in range(len(valid_predict)):
+            
+            if(valid_predict[i].argmax()==test_label[i].argmax()):
+                correct+=1
 
+        print("Test Accuracy: ",correct/len(test_label))
+
+        test_compare = valid_predict.argmax(axis=1)!=test_label.argmax(axis=1)
+
+        print("Test:" , valid_predict[test_compare],test_label[test_compare])
 
         if(input("keep training? (y/n): ") != 'y'):
             break
